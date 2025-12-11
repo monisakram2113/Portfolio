21b7a0b86aa4e043810fb66de915fd751b1632aa
monisakram2113 <monisakram5@gmail.com>
2025-12-11 15:08:00 +0530

    Update page.tsx
    Removed Force Dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import Home from "@/components/homecontent/Home";
 import { getHomePage } from "@/lib/api/api.service";
 import ErrorBoundary from "@/components/common/ErrorBoundary";
-export const dynamic = "force-dynamic";
-
 
 export default async function Page() {  
 
